fe26ff67be5583b3928b0d1ae5223d037d59bca7
Kaleb Holley <kalebholley@guest-2-00819.dhcp.unc.edu>
2016-10-29 12:58:28 -0400

    Kaleb 12:58

--- eComm/ViewController.swift
+++ eComm/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpButtons()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func setUpButtons(){
+        loginButton.layer.cornerRadius = 23
+        loginButton.layer.masksToBounds = true
+    
+    
+    }
 
 }
 
