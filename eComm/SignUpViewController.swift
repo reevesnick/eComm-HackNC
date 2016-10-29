f7998284935166835e7419fd205c2f5cd9eef9d8
Neegbeah Reeves <Nehemiahsal@aol.com>
2016-10-29 14:42:34 -0400

    Last Commit Continued

--- eComm/SignUpViewController.swift
+++ eComm/SignUpViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignUpViewController: UIViewController {
 
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var emailTextField: UITextField!
